64ffa04757cd85509afb9d8656b3e7aca644c470
Ronald Kinch <ronaldkinch@comcast.net>
2015-09-07 10:43:15 -0700

    Add theme html closes #10

--- test/features/use_zurb_test.rb
+++ test/features/use_zurb_test.rb
@@ -1,8 +1,14 @@
 require "test_helper"
 
-feature "UseZurb" do
-  scenario "has Zurb class" do
+feature "Use Zurb" do
+  scenario "has Zurb generated title" do
     visit root_path
     page.html.must_include 'foundation-rails'
+  end
+
+  scenario "has hero class" do
+    # foundation.zurb.com/templates/portfolio-theme.html
+    visit root_path
+    page.html.must_include 'class="hero"'
   end
 end
